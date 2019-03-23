5281bbe2d3d18fcabab5cc48122b23dc33f6c7fc
IvanTsypyshev <deadline.3012@gmail.com>
2019-03-23 18:43:56 +0700

    Theme2 Ex2

---
+++ src/Test2.java
@@ -0,0 +1,85 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class Test2 {
+
+    private AppiumDriver driver;
+
+    @Before
+
+    public void setUp() throws Exception{
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","6.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+
+        capabilities.setCapability("app","C:\\Users\\FBI\\IdeaProjects\\ApiumTraining\\JavaAppiumAutomation\\apks\\org.wikipedia.apk"); //Дом
+        //capabilities.setCapability("app","D:\\GitHub\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");  //Работа
+
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    }
+
+    @After
+
+
+    public void tearDawn(){
+        driver.quit();
+    }
+
+    @Test
+
+    public void findSearch(){
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Can Not Find Serch Bar",
+                5
+        );
+
+        WebElement title_element = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Can not Find Article title",
+                5);
+
+        String article_title = title_element.getAttribute("text");
+
+        Assert.assertEquals(
+                "Text field not empty",
+                "Search…",
+                article_title
+        );
+
+
+
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSecond){
+        WebDriverWait wait = new WebDriverWait(driver,timeoutInSecond);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSecond){
+
+        WebElement element = waitForElementPresent(by,error_message,timeoutInSecond);
+        element.click();
+        return element;
+    }
+}
